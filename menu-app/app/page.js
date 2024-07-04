dec751f54e0329b5f8c426d25b0ccee6e176d54f
Sheldon Small <ssmall9089@gmail.com>
2024-07-04 10:14:39 +0100

    Cleared default home page and added team placeholder text

---
+++ menu-app/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+
+    <div className="welcome-bx">
+      <h2>Team 13</h2>
+      <h1>Voyage 50</h1>
+      <h2>Tier 2</h2>
+    </div>
+    
+  );
+}
