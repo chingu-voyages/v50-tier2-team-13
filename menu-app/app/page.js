02334c39f9c264085d29edf984b9652719e59faf
Sheldon Small <ssmall9089@gmail.com>
2024-07-11 13:36:20 +0100

    Api call has been set up and selected category data stored

--- menu-app/app/page.js
+++ menu-app/app/page.js
@@ -1,14 +1,23 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch('https://menus-api.vercel.app/');
+  const data = await res.json();
+  const pizzas = data.pizzas;
+  const burgers = data.burgers;
+  const desserts = data.desserts;
+  const drinks = data.drinks;
+  const sandwiches = data.sandwiches;
+  const bestFoods = data['best-foods'];
+
+
   return (
-
-    <div className="welcome-bx">
-      <h2>Team 13</h2>
-      <h1>Voyage 50</h1>
-      <h2>Tier 2</h2>
+    <div>
+      <div className="welcome-bx">
+        <h2>Team 13</h2>
+        <h1>Voyage 50</h1>
+        <h2>Tier 2</h2>
+      </div>
     </div>
-    
   );
 }
