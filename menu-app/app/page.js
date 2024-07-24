0f5a8eadfb5fea42c3ff78dc1899ef07d12b8475
Sheldon Small <ssmall9089@gmail.com>
2024-07-24 12:44:33 +0100

    Users can now filter between categories on the map

--- menu-app/app/page.js
+++ menu-app/app/page.js
@@ -11,12 +11,32 @@ export default function Home({}) {
   const Map = dynamic(() => import("../components/Map"), { ssr: false });
 
   const [allMenuItems, setAllMenuItems] = useState([]);
+  const [pizzaMenuItems, setPizzaMenuItems] = useState([]);
+  const [burgerMenuItems, setBurgerMenuItems] = useState([]);
+  const [dessertsMenuItems, setDessertsMenuItems] = useState([]);
+  const [drinksMenuItems, setDrinksMenuItems] = useState([]);
+  const [sandwichesMenuItems, setSandwichesMenuItems] = useState([]);
+  const [popularMenuItems, setPopularMenuItems] = useState([]);
+
   const [currentMenuData, setCurrentMenuData] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
+
+  const [restaurants, setRestaurants] = useState([]);
+  const [allLocations, setAllLocations] = useState([]);
+
+  const filterCategory = (category) => {
+    console.log(category);
+    const locations = allLocations?.filter((item) => {
+      const itemid = item.id;
+      return category.some((cItem) => cItem.id === itemid);
+    });
+    setRestaurants(locations);
+  };
 
   const handleToggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
   const handleOnClick = (restaurant) => {
     const menuItems = allMenuItems.filter(
       (item) => item.name === restaurant.name
@@ -30,10 +50,33 @@ export default function Home({}) {
     handleToggleMenu();
   };
 
+  const loadAllLocations = () => {
+    const uniqueLocations = new Set();
+
+    const uniqueRestaurants = allMenuItems.filter((item) => {
+      const { latitude, longitude } = item;
+      if (latitude == null || longitude == null) {
+        console.warn("Invalid location:", item);
+        return false;
+      }
+      const locationKey = `${latitude},${longitude}`;
+      if (uniqueLocations.has(locationKey)) {
+        return false;
+      }
+      uniqueLocations.add(locationKey);
+      return true;
+    });
+
+    setRestaurants(uniqueRestaurants);
+    setAllLocations(uniqueRestaurants);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("https://menus-api.vercel.app/");
       const data = await res.json();
+
+      console.log("data:" , data)
 
       const allItems = [
         ...data.pizzas,
@@ -42,19 +85,36 @@ export default function Home({}) {
         ...data.drinks,
         ...data.sandwiches,
         ...data.bbqs,
+        ...data['best-foods'],
       ];
+      console.log("All Items" , allItems);
 
       setAllMenuItems(allItems);
+      setPizzaMenuItems(data.pizzas);
+      setBurgerMenuItems(data.burgers);
+      setDessertsMenuItems(data.desserts);
+      setDrinksMenuItems(data.drinks);
+      setSandwichesMenuItems(data.sandwiches);
+      setPopularMenuItems(data['best-foods']);
     };
 
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (allMenuItems.length > 0) {
+      loadAllLocations();
+    }
+  }, [allMenuItems]);
+
+
+
+
   return (
     <div>
       <Navbar />
-      <Categories/>
-      <Map allMenuItems={allMenuItems} handleOnClick={handleOnClick} />
+      <Categories pizzaMenuItems={pizzaMenuItems} burgerMenuItems={burgerMenuItems} dessertsMenuItems={dessertsMenuItems} drinksMenuItems={drinksMenuItems} sandwichesMenuItems={sandwichesMenuItems} popularMenuItems={popularMenuItems} filterCategory={filterCategory}/>
+      <Map restaurants={restaurants} handleOnClick={handleOnClick} />
       <Menu
         currentMenuData={currentMenuData}
         isOpen={isOpen}
--- menu-app/components/Map.js
+++ menu-app/components/Map.js
@@ -1,36 +1,14 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
-import { useEffect, useState } from "react";
+
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-const Map = ({ allMenuItems, handleOnClick }) => {
+const Map = ({ restaurants, handleOnClick }) => {
   const bounds = [
     [-85, -180],
     [85, 180],
   ];
-
-  const [restaurants, setRestaurants] = useState([]);
-
-  useEffect(() => {
-    const uniqueLocations = new Set();
-
-    const uniqueRestaurants = allMenuItems.filter((item) => {
-      const { latitude, longitude } = item;
-      if (latitude == null || longitude == null) {
-        console.warn("Invalid location:", item);
-        return false;
-      }
-      const locationKey = `${latitude},${longitude}`;
-      if (uniqueLocations.has(locationKey)) {
-        return false;
-      }
-      uniqueLocations.add(locationKey);
-      return true;
-    });
-
-    setRestaurants(uniqueRestaurants);
-  }, [allMenuItems]);
 
   var greenIcon = L.icon({
     iconUrl: "marker.png",
@@ -40,41 +18,47 @@ const Map = ({ allMenuItems, handleOnClick }) => {
   });
 
   return (
-    <MapContainer
-      className="map-container"
-      center={[45.476833, -89.679495]}
-      zoom={2}
-      scrollWheelZoom={true}
-      minZoom={2}
-      maxBounds={bounds}
-      maxBoundsViscosity={1.0}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-      />
-      {restaurants.map((restaurant) => (
-        <Marker
-          key={restaurant.id}
-          position={[restaurant.latitude, restaurant.longitude]}
-          icon={greenIcon}
+    <>
+      {restaurants && restaurants.length > 0 ? (
+        <MapContainer
+          className="map-container"
+          center={[45.476833, -89.679495]}
+          zoom={2}
+          scrollWheelZoom={true}
+          minZoom={2}
+          maxBounds={bounds}
+          maxBoundsViscosity={1.0}
         >
-          <Popup>
-            {restaurant.name} <br />
-            {restaurant.country} <br />
-            <button
-              className="view-menu-btn"
-              onClick={(e) => {
-                e.stopPropagation();
-                handleOnClick(restaurant);
-              }}
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          />
+          {restaurants.map((restaurant) => (
+            <Marker
+              key={restaurant.id}
+              position={[restaurant.latitude, restaurant.longitude]}
+              icon={greenIcon}
             >
-              View Menu
-            </button>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+              <Popup>
+                {restaurant.name} <br />
+                {restaurant.country} <br />
+                <button
+                  className="view-menu-btn"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleOnClick(restaurant);
+                  }}
+                >
+                  View Menu
+                </button>
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 };
 
