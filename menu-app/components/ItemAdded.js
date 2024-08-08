430bea574d48c62669212d5265cceb8a04e46bd9
mickeymic25 <mopokumensah18@gmail.com>
2024-08-08 19:10:32 +0100

    ItemAdded update

---
+++ menu-app/components/ItemAdded.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ItemAdded() {
+  return (
+    <div className="item-added-popup-container">
+      <div className="item-added-popup-content">
+        <p>Item added succesfully!</p>
+      </div>
+    </div>
+  );
+}
+
+export default ItemAdded;
--- menu-app/components/Modal.js
+++ menu-app/components/Modal.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/globals.css";
+import { IoIosArrowBack } from "react-icons/io";
+import ItemAdded from "./ItemAdded";
 
 export default function Modal({ setShowModal, modalItem, AddItemToOrder }) {
+  const [showItemAdded, setShowItemAdded] = useState(false);
   return (
     <div className="modal-overlay">
       <div className="modal-container">
-        <button onClick={() => setShowModal(false)}>Close</button>
+        <div className="go-back">
+          <IoIosArrowBack
+            className="back-arrow"
+            onClick={() => setShowModal(false)}
+          />
+          <p>Back</p>
+        </div>
+
         <div className="modal-content">
           <div className="modal-item-image-box">
             <img src={modalItem.img} alt={modalItem.id} />
@@ -17,10 +27,13 @@ export default function Modal({ setShowModal, modalItem, AddItemToOrder }) {
           <hr></hr>
           <div className="modal-button">
             <button
-              onClick={(e) =>{ 
+              onClick={(e) => {
                 e.preventDefault();
-                AddItemToOrder(modalItem)}}
-                >Add to Cart</button>
+                AddItemToOrder(modalItem);
+              }}
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
